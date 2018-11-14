4c386e08084b0fc8e19a47ede1d661221fa130e3
kbomb6 <baumanfamily4@gmail.com>
2018-11-13 20:45:56 -0700

    Initial Commit

---
+++ ReadFromFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace ReadFromFile
+{
+    class ReadFile
+    {
+        static void Main(string[] args)
+        {
+            var yourFile = File.ReadAllLines("fileNameHere");
+            foreach (string line in yourFile)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Would you like to run the Program? Press y to open it or n to exit the program");
+            var userInput = Console.ReadKey();
+
+
+            if (userInput.KeyChar == 'y')
+            {
+                ReadFile readFile = new ReadFile();
+                readFile.OpenApplication();
+            }
+        }
+
+        public void OpenApplication()
+        {
+            Process.Start("ApplicationPathHere");
+        }
+    }
+}
